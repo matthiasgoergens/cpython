6f882c8e0d49779a437df735d1e5235b9c0f896b
Matthias Goergens <matthias.goergens@gmail.com>
2022-09-28 13:17:38 +0800

    Disable warning about windows.h; not to be included in fix

--- Modules/_ctypes/cfield.c
+++ Modules/_ctypes/cfield.c
@@ -4,9 +4,9 @@
 
 #include "Python.h"
 // windows.h must be included before pycore internal headers
-#ifdef MS_WIN32
-#  include <windows.h>
-#endif
+// #ifdef MS_WIN32
+// #  include <windows.h>
+// #endif
 
 #include "pycore_bitutils.h"      // _Py_bswap32()
 #include "pycore_call.h"          // _PyObject_CallNoArgs()
